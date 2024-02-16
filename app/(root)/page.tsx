36ed31bd6ac12b036c5c4b2b0525dc65db11f422
aniket0711 <aniket.jadhav@techchefz.com>
2024-02-16 16:31:40 +0530

    Working on user modules

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,11 +3,11 @@
 // import Search from '@/components/shared/Search';
 import { Button } from "@/components/ui/button";
 // import { getAllEvents } from '@/lib/actions/event.actions';
-// import { SearchParamProps } from '@/types';
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home({ searchParams }: any) {
+export default async function Home({ searchParams }: SearchParamProps) {
   const page = Number(searchParams?.page) || 1;
   const searchText = (searchParams?.query as string) || "";
   const category = (searchParams?.category as string) || "";
